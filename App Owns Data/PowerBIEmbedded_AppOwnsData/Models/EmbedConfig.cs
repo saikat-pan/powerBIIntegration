d0e640d4801ad1d2da4df9c4233918f382bfa46a
Saikat Pan <saikat.pan@optum.com>
2017-12-13 11:44:02 +0530

    Base project

---
+++ App Owns Data/PowerBIEmbedded_AppOwnsData/Models/EmbedConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.PowerBI.Api.V2.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class EmbedConfig
+    {
+        public string Id { get; set; }
+
+        public string EmbedUrl { get; set; }
+
+        public EmbedToken EmbedToken { get; set; }
+
+        
+        public List<Employee> Employees { get; set; }
+        public List<string> ReportName { get; set; }
+        
+        public int MinutesToExpiration
+        {
+            get
+            {
+                //var minutesToExpiration = EmbedToken.Expiration.Value - DateTime.UtcNow;
+                return DateTime.UtcNow.Minute;
+            }
+        }
+
+        public string ErrorMessage { get; internal set; }
+    }
+}
---
+++ App Owns Data/PowerBIEmbedded_AppOwnsData/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class Employee
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string EmployeeName
+        {
+            get;
+            set;
+        }
+
+        public bool IsSelected
+        {
+            get;
+            set;
+        }
+
+    }
+}
